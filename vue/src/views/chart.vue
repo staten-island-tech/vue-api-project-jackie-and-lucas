862fc66a2094e035d78f4bb90fe35fcd80aced4f
jackiezzhen <jackiezhen31@gmail.com>
2024-03-06 09:23:17 -0500

    a

---
+++ vue/src/views/chart.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script setup>
+import { ref, onMounted} from "vue";
+await fetch ref("https://data.cityofnewyork.us/resource/jb7j-dtam.json");
+</script>
+
+<style lang="scss" scoped>
+
+</style>
