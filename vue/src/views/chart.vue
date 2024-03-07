c75b243dd030033b35c637af4c34dd1837300089
jackiezzhen <jackiezhen31@gmail.com>
2024-03-07 09:24:19 -0500

    api

--- vue/src/views/chart.vue
+++ vue/src/views/chart.vue
@@ -6,7 +6,13 @@
 
 <script setup>
 import { ref, onMounted} from "vue";
-await fetch ref("https://data.cityofnewyork.us/resource/jb7j-dtam.json");
+async function api(){
+ const api = await fetch("https://data.cityofnewyork.us/resource/jb7j-dtam.json");
+ const data = await api.json();
+}
+
+api();
+
 </script>
 
 <style lang="scss" scoped>
