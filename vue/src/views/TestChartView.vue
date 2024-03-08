b21f295ed94eeed24da576adb520c03524b9dc16
LKWQiu <124072151+LKWQiu@users.noreply.github.com>
2024-03-08 08:42:50 -0500

    Update TestChartView.vue

--- vue/src/views/TestChartView.vue
+++ vue/src/views/TestChartView.vue
@@ -1,29 +1,33 @@
 <template>
-    <Bar :data="TestData" />
-  </template>
-  
-  <script>
-  import { Bar } from 'vue-chartjs'
-  import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js'
-  
-  ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
-  
-  export default {
-    name: 'Test Data',
-    components: { Bar },
-    data() {
-      return {
-        TestData: {
-          labels: [ 'CAr1', 'CAr2', 'CAr3'],
-          datasets: [
-            {
-              label: 'CARS',
-              backgroundColor: '#f87979',
-              data: [160, 75, 22]
-            }
-          ]
-        }
-      }
+  <div class="container">
+    <Bar v-if="loaded" :data="chartData" />
+  </div>
+</template>
+
+<script>
+import { Bar } from 'vue-chartjs'
+import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js'
+
+ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
+
+export default {
+  name: 'BarChart',
+  components: { Bar },
+  data: () => ({
+    loaded: false,
+    chartData: null
+  }),
+  async mounted () {
+    this.loaded = false
+
+    try {
+      const { userlist } = await fetch('https://data.cityofnewyork.us/resource/jb7j-dtam.json')
+      this.chartdata = userlist
+
+      this.loaded = true
+    } catch (e) {
+      console.error(e)
     }
   }
-  </script>
+}
+</script>
