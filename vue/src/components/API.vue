eb4bda80d301976548e2b35e2e02e14e2651bb42
jackiezzhen <jackiezhen31@gmail.com>
2024-03-12 09:22:09 -0400

    help

---
+++ vue/src/components/API.vue
@@ -0,0 +1,6 @@
+<script setup>
+ export async function api(){
+    let link = await fetch("https://data.cityofnewyork.us/resource/jb7j-dtam.json")
+    let data = await link.json();
+   }
+</script>
--- vue/src/views/chart.vue
+++ vue/src/views/chart.vue
@@ -1,19 +1,20 @@
 <template>
     <div>
-        <p>`${item}`</p>
+        <p class="item"></p>
     </div>
 </template>
 
 <script setup>
-import Pullapi from "@/components/icon.vue";
+import thing from "@/components/API.vue";
 import { ref, onMounted} from "vue";
-const item = ref("");
+const k = ref(thing);
 async function api(){
  let api = await fetch("https://data.cityofnewyork.us/resource/jb7j-dtam.json");
  let data = await api.json();
  console.log(data[100])
  item.value = data[0]
 }
+console.log(k);
 
 onMounted(()=>{
     api();
