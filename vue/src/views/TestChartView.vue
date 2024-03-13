c6b9fd33185e0eb29053f9f3b9b48d46257f1983
LKWQiu <124072151+LKWQiu@users.noreply.github.com>
2024-03-13 09:19:39 -0400

    Update TestChartView.vue

--- vue/src/views/TestChartView.vue
+++ vue/src/views/TestChartView.vue
@@ -7,18 +7,20 @@
 <script setup>
 import { ref, onMounted} from "vue";
 const item = ref("");
-async function api(){
-let api = await fetch("https://data.cityofnewyork.us/resource/jb7j-dtam.json");
-let data = await api.json();
-item.ref = data[100];
-console.log(item)
 
+async function api(){
+  let api = await fetch("https://data.cityofnewyork.us/resource/jb7j-dtam.json");
+  let data = await api.json();
+
+for (let i = 0; i < 1000; i++) {  
+  item.ref = data[i];
+  console.log(item.ref.year)
+  }
 }
 
 onMounted(()=>{
   api();
 });
-
 </script>
 
 <style lang="scss" scoped>
