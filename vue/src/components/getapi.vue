6e65c3d6ca0a898e67bc7055170bd33ea2d87417
jackiezzhen <jackiezhen31@gmail.com>
2024-03-14 08:48:06 -0400

    upload

--- vue/src/components/getapi.vue
+++ vue/src/components/getapi.vue
@@ -6,13 +6,21 @@
 
 <script setup>
 import { ref, onMounted} from "vue";
+const item = ref("");
 async function api(){
-    let api = await fetch("https://data.cityofnewyork.us/resource/jb7j-dtam.json");
-    let data = await api.json();
-    console.log(data[0])
-    item.value = data  ;
-    console.log(item.value)
-   }
+ let api = await fetch("https://data.cityofnewyork.us/resource/jb7j-dtam.json");
+ let data = await api.json();
+ console.log(data[0])
+ item.value = data  ;
+ console.log(item.value)
+}
+
+
+onMounted(()=>{
+    api();
+
+});
+export {item}
 </script>
 
 <style lang="scss" scoped>
--- vue/src/views/chart.vue
+++ vue/src/views/chart.vue
@@ -7,14 +7,7 @@
 <script setup>
 import card from "@/components/icon.vue"
 import { ref, onMounted} from "vue";
-const item = ref("");
-async function api(){
- let api = await fetch("https://data.cityofnewyork.us/resource/jb7j-dtam.json");
- let data = await api.json();
- console.log(data[0])
- item.value = data  ;
- console.log(item.value)
-}
+import {item} from "@/components/getapi.vue"
 
 
 onMounted(()=>{
