59bb58c0e3f8f25a98b3aee0abfd45b568ffa18c
jackiezzhen <jackiezhen31@gmail.com>
2024-03-25 09:22:08 -0400

    now

--- vue/src/components/getapi.vue
+++
@@ -1,8 +0,0 @@
-<script>
-async function api(){
- let URL = await fetch("https://data.cityofnewyork.us/resource/jb7j-dtam.json");
- let data = await URL.json();
- console.log(data);
-}
-export {api};
-</script>
--- vue/src/views/AboutView.vue
+++ vue/src/views/AboutView.vue
@@ -45,3 +45,52 @@ export default {
   }
 }
 </script>
+
+<template>
+  <div class="container">
+    <Bar v-if="loaded" :data="2014chart" />
+  </div>
+</template>
+
+<script>
+import { Bar } from 'vue-chartjs'
+import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js'
+
+ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
+function year(yearNum, data){
+  const yearfilter = data.filter((num) => num.year == yearNum)
+  console.log(yearfilter)
+  const totaldeath = yearfilter.reduce( , yearnow) => {
+  const deathnum = yearnow.year;
+}
+}
+export default {
+  name: 'BarChart',
+  components: { Bar },
+  data: () => ({
+    loaded: false,
+    chartData: null
+  }),
+  async mounted () {
+    this.loaded = false
+
+    try {
+      const url = await fetch('https://data.cityofnewyork.us/resource/jb7j-dtam.json')
+      const data = await url.json();
+      const year2014 = year(2014, data)
+      const year2013 = year(2013, data)
+      const year2012 = year(2012, data)
+      const year2011 = year(2011, data)
+      const year2010 = year(2010, data)
+      const year2009 = year(2009, data)
+      const year2008 = year(2008, data)
+      const year2007 = year(2007, data)
+      this.chartdata = userlist
+
+      this.loaded = true
+    } catch (e) {
+      console.error(e)
+    }
+  }
+}
+</script>
