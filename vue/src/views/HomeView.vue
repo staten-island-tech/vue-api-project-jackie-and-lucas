d20dc13e7351ab15ebd655861669db33c323a54c
LKWQiu <124072151+LKWQiu@users.noreply.github.com>
2024-03-25 21:29:58 -0400

    smash smash smash

--- vue/src/views/HomeView.vue
+++ vue/src/views/HomeView.vue
@@ -5,7 +5,7 @@
   <main>
     <img class="image" src="https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExNGJjbW94dHgyNzNyd2xkeTF1N284OXdlMGFsa2RoMHYzbXR3a28xMyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/Oj8pUuT5FOpxHH9LIk/giphy.gif" ></img>
     <h3 class="text">
-      Here is some NYC data on the death rates
+      Here is some NYC data on the death rates for certain situations. Please click the different years on the left side to browse the charts.
     </h3>
   </main>
 </template>
@@ -22,6 +22,7 @@ main {
 .text{
   position: absolute;
   left: 60%;
-  top: 50%;
+  top: 45%;
+  width: 30vh;
 }
 </style>
