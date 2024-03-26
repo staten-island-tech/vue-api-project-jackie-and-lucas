c6076107e3d1a08dd10b7a986bdb642bf8a73d82
LKWQiu <124072151+LKWQiu@users.noreply.github.com>
2024-03-25 21:25:03 -0400

    asdasdas

--- vue/src/views/HomeView.vue
+++ vue/src/views/HomeView.vue
@@ -4,15 +4,24 @@
 <template>
   <main>
     <img class="image" src="https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExNGJjbW94dHgyNzNyd2xkeTF1N284OXdlMGFsa2RoMHYzbXR3a28xMyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/Oj8pUuT5FOpxHH9LIk/giphy.gif" ></img>
+    <h3 class="text">
+      Here is some NYC data on the death rates
+    </h3>
   </main>
 </template>
 
 <style scoped>
 main {
-  min-height: 100%;
-  min-width: 100%;
+  min-height: 20vh;
+  min-width: 100vh;
+  align-items: center;
 }
 .image{
   object-fit: contain;
+}
+.text{
+  position: absolute;
+  left: 60%;
+  top: 50%;
 }
 </style>
