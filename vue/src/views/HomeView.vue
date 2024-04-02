86b514538db06858e93bff9765a40fd8d0a7cd46
jackiezzhen <jackiezhen31@gmail.com>
2024-04-01 22:56:34 -0400

    Squashed commit of the following:
    commit 5286bb5a7a17d2af6d3f54e7a1fd2590a2741996
    Author: jackiezzhen <jackiezhen31@gmail.com>
    Date:   Mon Apr 1 22:55:25 2024 -0400
    
        s
    
    commit dcc3bcdad0983e880452c5dbdcde8600b567a4ef
    Merge: 30b72ea bf723f5
    Author: jackiezzhen <jackiezhen31@gmail.com>
    Date:   Mon Apr 1 19:26:26 2024 -0400
    
        Merge branch 'main' into jackie
    
    commit 30b72eaf1a0bc96938743d317834f3ab262f6a22
    Merge: b1974a2 c3c4123
    Author: jackiezzhen <jackiezhen31@gmail.com>
    Date:   Mon Apr 1 19:25:20 2024 -0400
    
        Merge branch 'main' into jackie
    
    commit b1974a2328ea9abe9278c962f5e59c801e0586a6
    Author: jackiezzhen <jackiezhen31@gmail.com>
    Date:   Mon Apr 1 18:20:32 2024 -0400
    
        c
    
    commit 98dff5f812d6d3941cc3a966dd64cfd5cd87fb02
    Merge: a28a97b da2408e
    Author: jackiezzhen <jackiezhen31@gmail.com>
    Date:   Mon Apr 1 15:04:34 2024 -0400
    
        Merge branch 'main' into jackie
    
    commit a28a97bb03f2076b7d383adee70a12d4377a0c19
    Author: jackiezzhen <jackiezhen31@gmail.com>
    Date:   Mon Apr 1 14:08:30 2024 -0400
    
        change
    
    commit 9860b62627a59aa75af9a2a4fad0022a874e7f9a
    Author: jackiezzhen <jackiezhen31@gmail.com>
    Date:   Thu Mar 28 09:22:26 2024 -0400
    
        A
    
    commit 3080fff1389147fb629b8a11ce49aa5fa06a896e
    Author: jackiezzhen <jackiezhen31@gmail.com>
    Date:   Tue Mar 26 09:24:02 2024 -0400
    
        AHHHHHHHHHHHH
    
    commit 59bb58c0e3f8f25a98b3aee0abfd45b568ffa18c
    Author: jackiezzhen <jackiezhen31@gmail.com>
    Date:   Mon Mar 25 09:22:08 2024 -0400
    
        now
    
    commit 01ba82630f0a3e8dd18b0d25c43bea737ee0e1d6
    Author: jackiezzhen <jackiezhen31@gmail.com>
    Date:   Fri Mar 22 09:23:26 2024 -0400
    
        .
    
    commit c455c078d75033ce81efd11ce174ce5a3d670327
    Author: jackiezzhen <jackiezhen31@gmail.com>
    Date:   Thu Mar 21 09:24:01 2024 -0400
    
        doigns me vchart thtigns
    
    commit 92d5a3e9df55807c748f45c4c5590447aeeae64d
    Author: jackiezzhen <jackiezhen31@gmail.com>
    Date:   Thu Mar 14 09:10:23 2024 -0400
    
        upload
    
    commit 6e65c3d6ca0a898e67bc7055170bd33ea2d87417
    Author: jackiezzhen <jackiezhen31@gmail.com>
    Date:   Thu Mar 14 08:48:06 2024 -0400
    
        upload
    
    commit 3e5f27aeb48371ed742e00d9bc5bd5bc08ed7561
    Author: jackiezzhen <jackiezhen31@gmail.com>
    Date:   Wed Mar 13 09:20:08 2024 -0400
    
        a
    
    commit 6f42eb91134c5b1bca175b07fa764d6668a9882f
    Author: jackiezzhen <jackiezhen31@gmail.com>
    Date:   Wed Mar 13 09:06:41 2024 -0400
    
        api

--- vue/src/views/HomeView.vue
+++ vue/src/views/HomeView.vue
@@ -1,7 +1,7 @@
 <template>
   <h6 class="apiName" style="color: black; ">New York City Leading Causes of Death</h6>
   <div class="container">
-    <Bar v-if="loaded" :data="chartData" />
+    <Bar class="deaths" v-if="loaded" :data="chartData" />
   </div>
 </template>
 
@@ -12,7 +12,7 @@ import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, Li
 
 ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
 
-function year(yearNum, api){
+function year(yearNum, api) {
   const yearfilter = api.filter((num) => num.year == yearNum)
   const totaldeath = yearfilter.reduce((totalnow, deathnow) => {
     if (deathnow.deaths !== ".") {
@@ -29,18 +29,20 @@ export default {
   components: { Bar },
   data: () => ({
     loaded: false,
-    chartData: {labels: [ '2007', '2008', '2009', '2010', '2011', '2012', '2013', '2014'],
-        datasets: [
-          {
-            label: 'Total Deaths Per Year',
-            backgroundColor: ['#ff0000', '#ff4000', '#ff8000', '#ffbf00', '#ffff00', '#bfff00', '#80ff00', '#40ff00'],
+    chartData: {
+      labels: ['2007', '2008', '2009', '2010', '2011', '2012', '2013', '2014'],
+      datasets: [
+        {
+          label: 'Total Deaths Per Year',
+          backgroundColor: ['#ff0000', '#ff4000', '#ff8000', '#ffbf00', '#ffff00', '#bfff00', '#80ff00', '#40ff00'],
           borderColor: `#000000`,
           borderWidth: 1,
-            data: []
-          }]}
+          data: []
+        }]
+    }
   })
   ,
-  async mounted () {
+  async mounted() {
     this.loaded = false
     try {
       const url = await fetch('https://data.cityofnewyork.us/resource/jb7j-dtam.json')
@@ -63,3 +65,15 @@ export default {
   }
 }
 </script>
+<style scoped>
+h6 {
+  font-size: 3rem;
+}
+
+.container {
+  width: 39rem;
+  height: auto;
+  position: relative;
+  place-items: center;
+}
+</style>
